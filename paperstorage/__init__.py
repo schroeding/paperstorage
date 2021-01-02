5637173ad1f775dee7b1cad104a50ce800b7e143
schroeding <schroeding@users.noreply.github.com>
2021-01-02 11:28:33 +0100

    Added docstring to init

--- paperstorage/__init__.py
+++ paperstorage/__init__.py
@@ -1,3 +1,9 @@
+"""
+PaperStorage
+
+A module to create paper backups for arbitrary data that are recoverable by simple means 
+"""
+
 __all__ = ['PaperStorage']
 
 from .paperstorage import PaperStorage
