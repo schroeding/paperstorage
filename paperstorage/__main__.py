ee812004fa64400fe024f94ecca5a12276d35862
schroeding <schroeding@users.noreply.github.com>
2021-01-03 13:25:29 +0100

    added experimental webcam restore

--- paperstorage/__main__.py
+++ paperstorage/__main__.py
@@ -22,7 +22,7 @@ def __restoreFromFolder(folder: str, ps: PaperStorage) -> PaperStorage:
 	Parameters:
 		folder (str):
 			path of target folder
-			must be a valid folder path or FileNotFound exceptions will be raised
+			must be a valid folder path or FileNotFound etc. exceptions will be raised
 		ps (PaperStorage):
 			PaperStorage object
 
@@ -38,6 +38,7 @@ def __restoreFromFolder(folder: str, ps: PaperStorage) -> PaperStorage:
 		for n in pyzbar.decode(_image):
 			ps.restoreFromQRString(n.data.decode('ascii'))
 	return ps
+
 
 def __interactiveFolder(_ps: PaperStorage) -> None:
 	while (True):
@@ -68,6 +69,86 @@ def __interactiveFolder(_ps: PaperStorage) -> None:
 	_file.close()
 
 
+def __interactiveWebcam(_ps: PaperStorage) -> None:
+	try:
+		print('')
+		import pygame.camera
+	except (ImportError):
+		print('Webcam restore uses PyGame to fetch webcam images, but PyGame is not installed. Please install it using \'python -m pip install pygame\' and try again.')
+		exit()
+	input('\nPress [Enter] to enable your webcam and to start the restore process. ')
+	
+	pygame.camera.init()
+	_cameras = pygame.camera.list_cameras()
+
+	if (len(_cameras) < 1):
+		print('\nNo webcam found. PyGame only supports certain cameras, so it\'s possible your webcam is not supported.')
+		exit()
+
+	_webcam = pygame.camera.Camera(_cameras[0], (1920, 1080), 'RGB')
+	_webcam.start()
+	_webcamWidth, _webcamHeight = _webcam.get_size()
+
+	print(f'\nUsing webcam \'{_cameras[0]}\' with resolution of {_webcamWidth} x {_webcamHeight}')
+	_ps = PaperStorage()
+
+	print('Please hold the first page into your webcam...\n(Press [CTRL] + [C] to abort)')
+
+	_lastCode = str()
+	_pageRead = False
+
+	while (not _pageRead):
+		try:
+			_image = PIL.Image.frombytes('RGB', (_webcamWidth, _webcamHeight), pygame.image.tostring(_webcam.get_image(), 'RGB', False))
+			_bwImage = PIL.ImageOps.grayscale(PIL.ImageOps.posterize(PIL.ImageOps.autocontrast(_image), 2))
+			for n in (pyzbar.decode(_image) + pyzbar.decode(_bwImage)):
+				if (n.data.decode('ascii') == _lastCode): continue
+				_lastCode = n.data.decode('ascii')
+				if (n.data.decode('ascii')[:6] != 'hcpb01'):
+					print('\nQR-Code detected, but this is not the first page.\nPlease hold the first page into your webcam...')
+					continue
+				_ps.restoreFromQRString(n.data.decode('ascii'))
+				print('\nFirst page read!')
+				_pageRead = True
+		except (KeyboardInterrupt):
+			quit()
+
+	try:
+		for i in _ps.getMissingDataBlocks():
+			print(f'\nPlease hold page {i + 2} into the webcam...')
+			_pageRead = False
+			while (not _pageRead):
+				_image = PIL.Image.frombytes('RGB', (_webcamWidth, _webcamHeight), pygame.image.tostring(_webcam.get_image(), 'RGB', False))
+				_bwImage = PIL.ImageOps.grayscale(PIL.ImageOps.posterize(PIL.ImageOps.autocontrast(_image), 2))
+				for n in (pyzbar.decode(_image) + pyzbar.decode(_bwImage)):
+					if (n.data.decode('ascii') == _lastCode): continue
+					_lastCode = n.data.decode('ascii')
+					_ps.restoreFromQRString(n.data.decode('ascii'))
+					if (i in _ps.getMissingDataBlocks()):
+						print(f'\nQR-Code detected, but this is not page {i + 2}.\nPlease hold page {i + 2} into the webcam...')
+						continue
+					print(f'\nPage {i + 2} read!')
+					_pageRead = True
+	except (KeyboardInterrupt):
+		quit()
+
+	_webcam.stop()
+
+	if (_ps._sha256 != hashlib.sha256(_ps.getData()).hexdigest()):
+		print(f'\nYour backup of \'{_ps._identifier}\' was restored, but something went wrong. (hash mismatch)\nThis should never happen. Please try to rescan all files into a fresh folder.\nYour file will still be saved, but is probably corrupt.')
+	else:
+		print(f'\nThat worked, your backup of \'{_ps._identifier}\' was restored completly!')
+	while (True):
+		_filename = input('Please choose a filename to save the restored file to: ')
+		try:
+			_file = open(_filename, 'wb+')
+		except (Exception):
+			print('Could not save the data to the specified filename. Please try something else.')
+		break
+	_file.write(_ps.getData())
+	_file.close()
+
+
 def main(argv) -> None:
 	"""Main method to start paperstorage in interactive mode / from the console (python -m paperstorage)
 	Do not use if you want to use paperstorage as a module.
@@ -75,13 +156,14 @@ def main(argv) -> None:
 	Takes arguments, returns nothing
 	"""
 	parser = argparse.ArgumentParser('paperstorage')
-	parser.add_argument('-o', dest='outputFilename', default='backup.pdf', help='filename to write output PDF file to', required=False)
-	parser.add_argument('-f', dest='inputFilename', help='read the specified file, otherwise stdin', required=False)
-	parser.add_argument('-id', dest='identifier', help='identifier that will be printed on the backup file', required=False)
+	parser.add_argument('-o', dest='outputFilename', metavar='filename', default='backup.pdf', help='filename to write to', required=False)
+	parser.add_argument('-f', dest='inputFilename', metavar='filename', help='read the specified file, otherwise stdin', required=False)
+	parser.add_argument('-id', dest='identifier', metavar='identifier', help='identifier that will be printed on the backup file', required=False)
 	parser.add_argument('--format', dest='format', choices=['A4','Letter'], default='A4', type=str, help='uses the specified format for the output PDF file')
 	parser.add_argument('--force-from-stdin', dest='forceStdin', action='store_true', default=False, help='forces a read from stdin, even with no piped data available', required=False)
-	parser.add_argument('--interactive-restore', dest='interactiveRestore', action='store_true', default=False, help='starts an interactive restore of a backup', required=False)
-	parser.add_argument('-b', dest='blocksize', choices=range(50, 1501, 50), metavar='[50-1500]' ,type=int, default=1500, help='use a custom block size between 50 bytes and (the default) 1500 bytes', required=False)
+	parser.add_argument('-restore', dest='restore', metavar='folder_path' ,default=None, type=str, help='restores a backup from scanned images inside a folder', required=False)
+	parser.add_argument('--interactiverestore', dest='interactiveRestore', action='store_true', default=False, help='starts an interactive restore of a backup', required=False)
+	parser.add_argument('-b', dest='blocksize', choices=range(50, 1501, 50), metavar='{50-1500}', type=int, default=1500, help='use a custom block size between 50 bytes and (the default) 1500 bytes', required=False)
 	arguments = parser.parse_args(argv)
 
 	_ps = None
@@ -124,12 +206,36 @@ def main(argv) -> None:
 					__interactiveFolder(_ps)
 				else:
 					if (input('Does your computer have a webcam and are you willing to use it to scan the pages? (yes / no) ').startswith('y')):
-						raise NotImplementedError('TODO')
+						__interactiveWebcam(_ps)
 					else:
 						print('\nYou have to use some other measure to make pictures of every page of the backup.\nPlease do so and save the resulting images into a single folder on this computer.\n')
 						input('Press [Enter] when you are done. ')
 						__interactiveFolder(_ps)
-						
+
+	elif (arguments.restore != None):
+
+		if (not os.path.isdir(argument.restore)):
+			print('Invalid path specified')
+			quit()
+
+		_ps = __restoreFromFolder(arguments.restore, _ps)
+		if (not _ps.isDataReady()):
+			if (_ps.getMissingDataBlocks() == []):
+				print('No data blocks found, doublecheck the path and try rescanning the pages')
+				quit()
+			else:
+				print(f'Some data blocks missing. Page(s) {",".join([str(n+2) for n in _ps.getMissingDataBlocks()])} are missing / unreadable\nRescan these pages and try again.')
+				quit()
+		if (arguments.outputFilename == 'backup.pdf'): arguments.outputFilename = (_ps._identifier if (_ps._identifier != None) else 'restored_file')
+		try:
+			_file = open(arguments.outputFilename, "wb")
+		except (Exception):
+			print(f'Could not write to \'{arguments.outputFilename}\'!')
+			quit()
+		_file.write(_ps.getData())
+		_file.close()
+		print(f'Saved restored file to \'{arguments.outputFilename}\'')
+
 	else:
 
 		if (arguments.format == 'Letter'):
@@ -160,7 +266,7 @@ def main(argv) -> None:
 		if (_ps.savePDF(arguments.outputFilename)):
 			print(f'Saved backup as \'{arguments.outputFilename}\'')
 		else:
-			print(f'Could not save backup as pdf file!')
+			print(f'Could not write to \'{argument.outputFilename}\'!')
 
 
 main(sys.argv[1:])
